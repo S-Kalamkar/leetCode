8c1a06eb7c6b69c29cb20e6ea191e20ce8180776
Saujwal Kalamkar <saujwal.kalamkar@gmail.com>
2025-01-23 22:04:58 -0600

    problem 1004

---
+++ 1004_Max_Consecutive_Ones_III.py
@@ -0,0 +1,13 @@
+def longestOnes(nums: list[int], k: int) -> int:
+        n = len(nums)
+        l = 0
+        for r in range(n):
+            if nums[r] == 0:
+                k -= 1
+                
+            if k < 0:
+                if nums[l] == 0:
+                    k += 1
+                l += 1
+
+        return r - l + 1
