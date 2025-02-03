af94a060dcf67855e626c8106a3e0af1dcc430d1
Saujwal Kalamkar <saujwal.kalamkar@gmail.com>
2025-02-03 12:00:37 -0600

    problem 2011

---
+++ 2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,8 @@
+def finalValueAfterOperations(operations: list[str]) -> int:
+        x = 0
+        for operation in operations:
+            if operation == "X++" or operation == "++X":
+                x += 1
+            else:
+                x -= 1
+        return x
